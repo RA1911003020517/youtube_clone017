90a1b570e8e7def6116c62e9c48443ab64ca4523
RA1911003020517 <mr4131@srmist.edu.in>
2022-08-18 18:14:40 +0530

    first changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ fetch(video_http + new URLSearchParams({
     key: api_key,
     part: 'snippet',
     chart: 'mostPopular',
-    maxResults: 50,
+    maxResults: 100,
     regionCode: 'IN'
 }))
 .then(res => res.json())
